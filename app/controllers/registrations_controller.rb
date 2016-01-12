d5c4cfc732f0e33e8bf94dcfc065861c4ddad7a0
Alexandr Orlov <morozhunce@gmail.com>
2016-01-12 14:40:06 +0200

    add fields to user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def create
+    @new_user = User.new(sign_up_params)
+    @new_user.contacts = [params[:phone], params[:skype], params[:facebook], params[:vk]].join('|')
+    @new_user.save
+
+    redirect_to root_path
+  end
+
 private
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :stuck_out_tongue_winking_eye, :password, :stuck_out_tongue_winking_eye, :password_confirmation)
+    params.require(:user).permit(:name, :info, :contacts, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :stuck_out_tongue_winking_eye, :password, :stuck_out_tongue_winking_eye, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :info, :contacts, :email, :password, :password_confirmation, :current_password)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112100629) do
+ActiveRecord::Schema.define(version: 20160112111701) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255,   default: "", null: false
+    t.string   "encrypted_password",     limit: 255,   default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,     default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
@@ -28,8 +28,11 @@ ActiveRecord::Schema.define(version: 20160112100629) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email",      limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.string   "name",                   limit: 255
+    t.text     "info",                   limit: 65535
+    t.string   "contacts",               limit: 255
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
