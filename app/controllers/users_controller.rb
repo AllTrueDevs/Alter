f3bd57f9178d01533d233ffbc773578a44b21545
Alexandr Orlov <morozhunce@gmail.com>
2016-01-13 16:44:36 +0200

    routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :requests
   get 'home/index'
   root 'home#index'
-  devise_for :users, controllers: { registrations: "registrations"}
+  devise_for :users, controllers: { registrations: "registrations" }
+  get '/id:id', to: 'users#show'
+  get '/users', to: 'users#index'
 
 end
