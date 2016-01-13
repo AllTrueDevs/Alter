c7bc190ecf7f659ebad8bfccdd4f4e27996e88a3
Alexandr Orlov <morozhunce@gmail.com>
2016-01-13 17:35:49 +0200

    new models

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,6 @@
 class Request < ActiveRecord::Base
   belongs_to :user
+  has_many :required_items
   has_attached_file :photo, default_url: "missing-photo.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
