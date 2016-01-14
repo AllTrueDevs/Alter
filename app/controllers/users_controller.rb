d9f9ce4ba1f3666f14c959aa7d90a39e49bf1e2a
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-01-14 15:24:13 +0200

    Delete comments and not used views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,4 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def ban(user)
-    if (user.role == 'author')
-      user.role = 'banned'
-    else
-      user.role = 'author'
-    end
-    redirect_to :back
-  end
-
 end
