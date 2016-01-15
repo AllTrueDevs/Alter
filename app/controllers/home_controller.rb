eeaca35fd930e1953d14d98471ae923bc5f98af6
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-01-15 10:22:31 +0200

    Rework display notification

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    @requests = Request.where(user_id: current_user.id)
-    @notifications = Notification.where(request_id: @requests)
+    if user_signed_in?
+      @notifications = Notification.where(request_id: Request.where(user_id: current_user.id))
+    end
   end
 
 end
