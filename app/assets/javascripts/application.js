300092e5e62f95f04e8824dc8682c777d2974315
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-01-29 13:52:30 +0200

    Chartrick without network

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jsapi
 //= require chartkick
 //= require jquery
 //= require bootstrap-sprockets
