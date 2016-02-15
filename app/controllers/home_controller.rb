de891d9f5717d2653d4c900b1d4d1f458e609957
Alexandr Orlov <morozhunce@gmail.com>
2016-02-15 12:06:47 +0200

    part of home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,12 @@ class HomeController < ApplicationController
   def index
     @actual_request = Request.actual.last
     @top_users = User.all.sort_by {|post| post.helped_items_count }.reverse.take(3)
-    @requests = Request.actual.limit(5).order(created_at: :desc)
+    @requests = []
+    loop do
+      request = Request.actual.offset(Random.rand(Request.actual.size)).first
+      @requests << request unless @requests.include? request
+      break if @requests.size == 3
+    end
   end
 end
 
