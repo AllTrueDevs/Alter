57af40dcdfdea4ac757c6f87d427a50ec7f3b6d3
Alexandr Orlov <morozhunce@gmail.com>
2016-04-04 17:30:02 +0300

    init

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '4.2.4'
 #############################
 ##  Database gems
 #############################
-gem 'mysql2', '~>0.3.20'
+gem 'pg'
 
 #############################
 ### Authentication
@@ -33,8 +33,6 @@ gem 'kaminari'
 gem 'chartkick'
 gem 'bootstrap-filestyle-rails'
 
-gem 'sdoc', '~> 0.4.0', group: :doc
-
 #############################
 ### Backend
 #############################
@@ -56,3 +54,7 @@ group :development do
   gem 'spring'
 end
 
+group :production do
+  gem 'rails_12factor', '0.0.2'
+  gem 'aws-sdk-v1'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -13,108 +13,100 @@
 
 ActiveRecord::Schema.define(version: 20160209161045) do
 
-  create_table "accepted_items", force: :cascade do |t|
-    t.integer  "decision_id",      limit: 4
-    t.integer  "required_item_id", limit: 4
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-  create_table "authentications", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.string   "provider",   limit: 255
-    t.string   "uid",        limit: 255
-    t.string   "index",      limit: 255
-    t.string   "create",     limit: 255
-    t.string   "destroy",    limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+  create_table "accepted_items", force: :cascade do |t|
+    t.integer  "decision_id"
+    t.integer  "required_item_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "color",      limit: 4
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "color"
   end
 
   create_table "decisions", force: :cascade do |t|
-    t.integer  "helper_id",   limit: 4
-    t.integer  "request_id",  limit: 4
-    t.string   "status",      limit: 255,   default: "new"
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
-    t.text     "description", limit: 65535
+    t.integer  "helper_id"
+    t.integer  "request_id"
+    t.string   "status",      default: "new"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.text     "description"
   end
 
   create_table "helped_items", force: :cascade do |t|
-    t.integer  "count",       limit: 4, default: 1
-    t.integer  "user_id",     limit: 4
-    t.integer  "category_id", limit: 4
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.integer  "count",       default: 1
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "notifications", force: :cascade do |t|
-    t.string   "status",         limit: 255, default: "new"
-    t.integer  "user_id",        limit: 4
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
-    t.integer  "message_type",   limit: 4
-    t.integer  "reason_user_id", limit: 4
-    t.integer  "request_id",     limit: 4
+    t.string   "status",         default: "new"
+    t.integer  "user_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "message_type"
+    t.integer  "reason_user_id"
+    t.integer  "request_id"
   end
 
   create_table "requests", force: :cascade do |t|
-    t.string   "name",               limit: 255
-    t.text     "description",        limit: 65535
-    t.integer  "user_id",            limit: 4
-    t.datetime "created_at",                                          null: false
-    t.datetime "updated_at",                                          null: false
-    t.string   "photo_file_name",    limit: 255
-    t.string   "photo_content_type", limit: 255
-    t.integer  "photo_file_size",    limit: 4
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
-    t.string   "status",             limit: 255,   default: "actual"
+    t.string   "status",             default: "actual"
   end
 
   create_table "required_items", force: :cascade do |t|
-    t.integer  "request_id",  limit: 4
-    t.integer  "category_id", limit: 4
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.integer  "request_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255,   default: "",       null: false
-    t.string   "encrypted_password",     limit: 255,   default: "",       null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "",       null: false
+    t.string   "encrypted_password",     default: "",       null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,     default: 0,        null: false
+    t.integer  "sign_in_count",          default: 0,        null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
-    t.datetime "created_at",                                              null: false
-    t.datetime "updated_at",                                              null: false
-    t.string   "name",                   limit: 255
-    t.text     "info",                   limit: 65535
-    t.string   "avatar_file_name",       limit: 255
-    t.string   "avatar_content_type",    limit: 255
-    t.integer  "avatar_file_size",       limit: 4
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
+    t.string   "name"
+    t.text     "info"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "role",                   limit: 255,   default: "author"
-    t.string   "skype",                  limit: 255
-    t.string   "phone",                  limit: 255
-    t.string   "vkontakte",              limit: 255
-    t.string   "vkontakte_name",         limit: 255
-    t.string   "facebook",               limit: 255
-    t.string   "facebook_name",          limit: 255
+    t.string   "role",                   default: "author"
+    t.string   "skype"
+    t.string   "phone"
+    t.string   "vkontakte"
+    t.string   "vkontakte_name"
+    t.string   "facebook"
+    t.string   "facebook_name"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
