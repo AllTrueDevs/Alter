dec548271ac88f0dae0d15d8b8549d5e33fbd771
Alexandr Orlov <morozhunce@gmail.com>
2016-04-14 16:36:40 +0300

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,5 @@
 module ApplicationHelper
-  def default_page?
-    controller_name != 'page' && action_name != 'home'
-  end
-
   def home_page?
-    puts "keeeek: #{controller_name} leeeeel: #{action_name} #{controller_name == 'page' && action_name == 'home'}"
     controller_name == 'page' && action_name == 'home'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411171434) do
+ActiveRecord::Schema.define(version: 20160414120702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,13 @@ ActiveRecord::Schema.define(version: 20160411171434) do
     t.string   "name"
     t.text     "body"
     t.string   "src"
-    t.string   "photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "tags"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "categories", force: :cascade do |t|
