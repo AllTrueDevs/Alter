b25d7540977bafa97ee20be3f6885274d382d34e
Alexandr Orlov <morozhunce@gmail.com>
2016-05-04 19:48:40 +0300

    change password - part3

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,7 +2,9 @@ module DeviseHelper
     def devise_error_messages!
       return '' if resource.errors.empty?
       messages = resource.errors.full_messages.map { |msg| content_tag(:p, msg) }.join
-
+      sentence = I18n.t('errors.messages.not_saved',
+                             count: resource.errors.count,
+                             resource: resource.class.model_name.human.downcase)
       html = <<-HTML
         <div class="alert-error alert-block">
           #{messages}
