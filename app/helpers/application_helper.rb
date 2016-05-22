3caa0c6781012298090a14754415a22451397734
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-05-22 16:21:43 +0300

    flashes level

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def home_page?
     controller_name == 'page' && action_name == 'home'
   end
+
+  def flash_class(level)
+    case level.to_sym
+    when :notice then 'alert alert-info'
+    when :success then 'alert alert-success'
+    when :error then 'alert alert-danger'
+    when :alert then 'alert alert-warning'
+    end
+  end
 end
