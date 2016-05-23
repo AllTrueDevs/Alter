71c5d28f3479486d303efe28b0b8d5e6342e2d75
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-05-23 14:38:02 +0300

    refact my fucking code

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ActiveRecord::Base
   has_many :helped_items
   validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   validates :color, presence: true
+
+  def id_with_color
+    "#{id}::#{color}"
+  end
 end
