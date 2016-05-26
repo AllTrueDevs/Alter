4bff2af31c07e1f56b673183e5fb01e8becaa283
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-05-26 13:39:18 +0300

    attachm

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -5,6 +5,7 @@ class AttachmentsController < ApplicationController
   def download
     @attachment = Attachment.find(params[:id])
     data = open(@attachment.content.url)
+    send_file "http://alter-assets.s3.amazonaws.com/#{@attachment.content.path}", filename: @attachment.content_file_name, type: @attachment.content_content_type, disposition: :attachment
     send_data data.read, filename: @attachment.content_file_name, type: @attachment.content_content_type, disposition: 'attachment', stream: 'true', buffer_size: '4096'
   end
 end
