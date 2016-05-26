a7a3e19f626d3ee32b7438217b187795dea62ee5
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-05-27 00:25:22 +0300

    notifications page

--- app/views/notifications/destroy.js
+++ app/views/notifications/destroy.js
@@ -1,18 +1,4 @@
-$('.delete-notification').bind('ajax:success', function () {
-    if (hasClass($(this).parent().parent().parent().parent().parent(), 'notification-new')) {
-        if ($('#new-notifications-count').length > 0) {
-            $('#new-notifications-count').text(parseInt($('#new-notifications-count').text()) - 1);
-            if (parseInt($('#new-notifications-count').text()) === 0) {
-                $('#new-notifications-count').remove();
-            }
-        }
-    }
-    $(this).closest('.notification-in-list').fadeOut(function () {
-        $(this).remove();
-        if ($('.notification-in-list').length === 0) $('.notifications').append("<h5 class='text-center'> Сповіщень не знайдено</h5>");
-    });
+$("#notification-"+"<%= @notification.id %>").fadeOut(function() {
+    $(this).remove();
+    if( $('.notification').length === 0  )  $('.notification-index').append("<h5 class='text-center'> Сповіщень не знайдено</h5>");
 });
-
-function hasClass(element, cls) {
-    return (' ' + element.prop('class') + ' ').indexOf(' ' + cls + ' ') > -1;
-}
