90949fe5249ed911688f62b0a7d5b950fd9afb81
Alexandr Orlov <morozhunce@gmail.com>
2016-05-26 16:14:43 +0300

    fix

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,5 @@
 class Attachment < ActiveRecord::Base
-  include AmazonModule
+
   belongs_to :message
 
   {
@@ -41,7 +41,10 @@ class Attachment < ActiveRecord::Base
   end
 
   before_destroy do
-    clear_s3_object(self.content)
+    s3 = AWS::S3.new(access_key_id: ENV['AWS_ACCESS_KEY_ID'], secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
+    bucket = s3.buckets['alter-assets']
+    object = bucket.objects[content.path[1..-1]]
+    object.delete
   end
 
 end
