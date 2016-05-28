20edc4965d9fa6367a09a810c37ecb5350dde680
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-05-28 13:07:02 +0300

    decrement notif_count after destroy new

--- app/views/notifications/destroy.js
+++ app/views/notifications/destroy.js
@@ -1,4 +1,12 @@
 $("#notification-"+"<%= @notification.id %>").fadeOut(function() {
     $(this).remove();
+    if("<%= @notification.status %>" === "new"){
+        if ($('#new-notifications-count').length > 0) {
+            $('#new-notifications-count').text(parseInt($('#new-notifications-count').text()) - 1);
+            if (parseInt($('#new-notifications-count').text()) === 0) {
+                $('#new-notifications-count').remove();
+            }
+        }
+    }
     if( $('.notification').length === 0  )  $('.notification-index').append("<h5 class='text-center'> Сповіщень не знайдено</h5>");
 });
