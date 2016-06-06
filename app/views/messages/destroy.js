edcba1505ff91307e6be7f423acd8bed54889b12
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-06-06 10:36:11 +0300

    some chngs

--- app/views/messages/destroy.js
+++ app/views/messages/destroy.js
@@ -2,7 +2,7 @@ setTimeout(function(){
     $("#post-"+"<%= @post.id %>").fadeOut(function() {
         $(this).remove();
         if($('.post-in-list').length === 0)
-            $('.posts').append("<h5 class='text-center'>На стіні немає записів</h5>");
+            $('.posts').append("<p class='nothing-to-show'>На стіні немає записів</p>");
         $posts_count = "<%= @posts.size %>";
         $posts_count = (parseInt($posts_count) != 0)? '(' + $posts_count + ')' : '';
         $('#wall-counter').text($posts_count)
