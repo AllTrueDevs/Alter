53be989431a1d52d47ce2d11d98dd684446a2b6a
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-06-19 14:15:30 +0300

    request wall

--- app/views/messages/new_post.js
+++ app/views/messages/new_post.js
@@ -1,6 +1,6 @@
 $posts_count = "<%= @post.request.posts.size %>";
 $('#wall-counter').text('(' + $posts_count + ')');
-$("<%= escape_javascript(render partial: 'messages/post', locals: { post: @post }) %>").prependTo('.posts');
+$("<%= escape_javascript(render partial: 'messages/post', locals: { post: @post }) %>").prependTo('.post-index');
 $('.posts h5').remove();
 $('.message-form .fields').each(function() {
     return $(this).remove();
