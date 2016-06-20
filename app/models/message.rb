9ebbfadf8c504dd3d3553d94a52c4abc487a720a
Alexandr Orlov <morozhunce@gmail.com>
2016-06-20 18:32:36 +0300

    kek

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  include Wordable
+  include Wordable, AmazonModule
   has_many :attachments, dependent: :destroy
   belongs_to :receiver,  class_name: 'User', foreign_key: 'receiver_id'
   belongs_to :sender,  class_name: 'User', foreign_key: 'sender_id'
